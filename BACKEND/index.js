a0baa4d259d3073e0a755faa2c6941dfec5413f4
PRAVEENTH R <praveenthravichandran@gmail.com>
2023-11-10 20:29:33 +0530

    User Authentication

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,17 +1,18 @@
 var express = require('express');
 const connectDb = require('./db');
+var signinRouter = require('./routes/signin');
+const cors = require('cors');
 
 var app = express();
 const port = 4000;
+app.use(express.json());
+app.use(cors({ origin: '*' }));
 connectDb();
 
 app.get('/', (req, res) => {
     res.send('hello world');
 });
-
-app.get('/sayhi', (req, res) => {
-    res.send('All is well');
-});
+app.use('/signin', signinRouter);
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
